17c38f47a1c4d6def70557d68f0d79d420508af4
Surinder Singh <166700453+Whitedevil0237@users.noreply.github.com>
2025-11-28 20:10:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Simple contact form submission alert
 const form = document.querySelector('.contact-form');
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  alert('Thank you for reaching out! We’ll get back to you soon.');
-  form.reset();
+ 
 });
+
